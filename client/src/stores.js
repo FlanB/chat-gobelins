092f35a691988ec8d2570068740e19e8585387fe
FlanB <clement.renou@mmibordeaux.com>
2022-10-07 13:13:44 +0200

    🔧: change server config

--- client/src/stores.js
+++ client/src/stores.js
@@ -1,10 +1,9 @@
 import { io } from "socket.io-client"
 import { writable } from "svelte/store"
 
-export const socket = io("http://localhost:3001")
+export const socket = io("https://chat-gobelins.herokuapp.com/")
 socket.on("connect", () => {
   console.log("connected")
-  
 })
 socket.on("disconnect", () => {
   console.log("disconnected")
